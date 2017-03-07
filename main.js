e367483682ea79348153d38b9c09bd893d21f84d
Viv <vivacity.bright@gmail.com>
2017-03-06 18:59:21 -0600

    Fixed a dumb thing oops lmao

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 const fs = require("fs");
 const Discord = require('discord.js');
-const github = require("github.js")
 var data = JSON.parse(fs.readFileSync("data.json"));
 const login = JSON.parse(fs.readFileSync("login.json"));
 const client = new Discord.Client();
